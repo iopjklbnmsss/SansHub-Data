8af025a8e9a19f7a0376d3b6d286c75e34329952
SansHub <1208931506@qq.com>
2025-05-30 18:37:27 +0800

    Update and rename README.md to users.lua

---
+++ users.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        username = "admin",
+        password = "sanshub123"
+    },
+    {
+        username = "chen3xiao",
+        password = "owner123"
+    },
+    {
+        username = "abai",
+        password = "admin456"
+    }
+}
